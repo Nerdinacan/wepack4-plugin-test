0c36faa0c844fe3ab0592cfc0957a53f12ac6502
Mason Houtz <mason@Masons-MBP.domain>
2018-09-07 08:47:51 -0700

    Cleanup distractions

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let config = {
-    mode: "production",
-    // optimization: {
-    //     minimize: false
-    // },
-    // entry: {
-    //     vendor: [
-    //         "libs/jquery/jquery.migrate",
-    //         "libs/jquery/jquery.autocomplete",
-    //         "libs/jquery/jquery.event.hover",
-    //         "libs/jquery/jquery.event.drag",
-    //         "libs/jquery/jquery.mousewheel",
-    //         "libs/jquery/jquery.form",
-    //         "libs/jquery/jquery.rating",
-    //         "libs/jquery/select2",
-    //         "libs/jquery.sparklines",
-    //         "libs/jquery/jquery-ui",
-    //         "libs/jquery/jstorage",
-    //         "libs/jquery/jquery.complexify"
-    //     ]
-    // },
-    // output: {
-    //     filename: '[name].js'
-    // },
     module: {
         rules: [
-            // {
-            //     test: require.resolve('jquery'),
-            //     use: [{
-            //         loader: 'expose-loader',
-            //         options: 'jQuery'
-            //     }, {
-            //         loader: 'expose-loader',
-            //         options: '$'
-            //     }]
-            // },
-            // {
-            //     test: require.resolve("jquery-autocomplete"),
-            //     use: "imports-loader?$=>jquery"
-            // },
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
