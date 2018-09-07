ce38d229eb93d4bc7a7efa31d64b7c141bae42cd
Mason Houtz <mason@Masons-MacBook-Pro.local>
2018-09-07 11:56:14 -0700

    Added select2 test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 import $ from "jquery";
 import "jquery-autocomplete";
 import "jquery-autocomplete/jquery.autocomplete.css";
+import "select2";
+import "select2/dist/css/select2.css";
 import "./test.css";
 
-const tags = [
-    "ActionScript",
-    "AppleScript",
-    "Asp",
-    "BASIC",
-    "C",
-    "C++",
-    "Clojure",
-    "COBOL"
-];
 
-$(function () {
+function initAutoComplete() {
+
+    console.log("initAutoComplete");
+
+    let tags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL"
+    ];
+
     $("#youCompleteMe").autocomplete({
         source: [tags]
     });
+}
+
+function initSelect2() {
+    console.log("initSelect2");
+    $(".select2Test").select2();
+}
+
+
+$(function () {
+    console.log("Init ran");
+    initAutoComplete();
+    initSelect2();
 });
